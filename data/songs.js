788f82b6597281f1d76264dec4fed16d46f3e8fe
ElifnurSahin <45545127+yilmazelifnur@users.noreply.github.com>
2023-09-25 12:19:10 +0300

    add cover image

--- data/songs.js
+++ data/songs.js
@@ -1,47 +1,47 @@
 export const songs = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    songTitle: "Şarkı 1",
+    songTitle: "Grooving Infinity",
     artist: "HikoBeat",
     album: "Album 1",
     duration: "3:45",
-    coverImage: "song1.jpg",
+    coverImage: "../images/sarki1.png",
     priceCents: 2099
   },
   {
     id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    songTitle: "Şarkı 2",
+    songTitle: "Purple Night",
     artist: "HikoBeat",
     album: "Album 2",
     duration: "4:10",
-    coverImage: "song2.jpg",
+    coverImage: "../images/sarki2.png",
     priceCents: 2099
   },
   {
     id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
-    songTitle: "Şarkı 3",
+    songTitle: "Karma",
     artist: "HikoBeat",
     album: "Album 1",
     duration: "2:55",
-    coverImage: "song3.jpg",
+    coverImage: "../images/sarki3.png",
     priceCents: 2099
   },
   {
     id: "54e0eccd-8f36-462b-b68a-8182611d9add",
-    songTitle: "Şarkı 4",
+    songTitle: "Giveaway Battle",
     artist: "HikoBeat",
     album: "Album 3",
     duration: "3:30",
-    coverImage: "song4.jpg",
+    coverImage: "../images/sarki4.png",
     priceCents: 2099
   },
-  {
-    id: "4f4fbcc2-4e72-45cc-935c-9e13d79cc57f",
-    songTitle: "Şarkı 5",
-    artist: "HikoBeat",
-    album: "Album 2",
-    duration: "4:20",
-    coverImage: "song5.jpg",
-    priceCents: 2099
-  }
+  // {
+  //   id: "4f4fbcc2-4e72-45cc-935c-9e13d79cc57f",
+  //   songTitle: "Sofia",
+  //   artist: "HikoBeat",
+  //   album: "Album 2",
+  //   duration: "4:20",
+  //   coverImage: "../images/sarki5.png",
+  //   priceCents: 2099
+  // }
 ];
--- scripts/hikobeat.js
+++ scripts/hikobeat.js
@@ -2,17 +2,15 @@ import { playMusic , formatCurrency } from "../data/functions.js";
 import { songs } from "../data/songs.js";
 
 let songsHTML= '';
-
-
-
-    songs.forEach((song) => {
+   songs.forEach((song) => {
       songsHTML += `
         <div class="song-container">
           <div class="song-image-container">
-            <img class="song-image" src="${song.coverImage}" alt="${song.name}">
+            <img class="song-image" src="${song.coverImage}">
+            
           </div>
           <div class="song-name limit-text-to-2-lines">
-            ${song.name}
+            ${song.songTitle}
           </div>
           <div class="song-price">
             $${formatCurrency(song.priceCents)}
@@ -20,25 +18,5 @@ let songsHTML= '';
         </div>
       `;
     });
-
-// songs.forEach((song) =>{
-//   songsHTML += `
-//       <div class="song-container">
-//       <div class="song-image-container">
-//         <img class="song-image"
-//           src="${song.coverImage}">
-//       </div>
-
-//       <div class="song-name limit-text-to-2-lines">
-//        ${song.name}
-//       </div>
-
-//       <div class="song-price">
-//         $${formatCurrency(song.priceCents)}
-//       </div>
-      
-//     </div>
-//   ` ;
-// });
 
 document.querySelector('.js-songs-grid').innerHTML =songsHTML;
